c0fd4ad2a79247b161aa968f49cb17a842434af8
retardpa1n <74207477+retardpa1n@users.noreply.github.com>
2021-09-25 14:09:18 +0300

    Add files via upload

---
+++ WebhookUploader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+using Discord.Webhook;
+
+namespace WebhookUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "discord webhook uploader made by somestuff";
+            Console.Write("Enter WebhookUrl: ");
+            string webhook = Console.ReadLine();
+
+            DiscordWebhookClient client = new DiscordWebhookClient(webhook);
+                new Thread(() => {
+                    foreach (FileInfo file in new DirectoryInfo(Directory.GetCurrentDirectory() + "/Data/").GetFiles())
+                    {
+                        Console.WriteLine(file.Name);
+                        client.SendFileAsync(Directory.GetCurrentDirectory() + "/Data/" + file.Name, null, false);
+
+                        Thread.Sleep(5000);
+                    }
+                }).Start();
+            Console.ReadLine();
+        }
+    }
+}
